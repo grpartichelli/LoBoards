a2e10908c5b78b2c5863ec46a90ce9e09514dbbf
rafinhabusatta <lacerdabusatta@gmail.com>
2023-03-02 21:42:00 -0300

    Inserção da classe TsoroYematatu; criação da lógica de vitória; criação das regras

--- app/src/main/java/com/marcoantonioaav/lobogames/PreGameActivity.java
+++ app/src/main/java/com/marcoantonioaav/lobogames/PreGameActivity.java
@@ -15,6 +15,7 @@ import com.marcoantonioaav.lobogames.game.Alquerque;
 import com.marcoantonioaav.lobogames.game.Game;
 import com.marcoantonioaav.lobogames.game.Tapatan;
 import com.marcoantonioaav.lobogames.game.TicTacToe;
+import com.marcoantonioaav.lobogames.game.TsoroYematatu;
 import com.marcoantonioaav.lobogames.player.agent.Agent;
 
 import java.util.HashMap;
@@ -34,6 +35,7 @@ public class PreGameActivity extends AppCompatActivity {
         put(new Tapatan().getName(), new Tapatan());
         put(new TicTacToe().getName(), new TicTacToe());
         put(new Alquerque().getName(), new Alquerque());
+        put(new TsoroYematatu().getName(), new TsoroYematatu());
     }};
 
     @Override
