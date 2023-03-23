00c25d44145ca24d705d043c2107c0f85343f80f
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2023-03-22 23:12:51 -0300

    Nivelamento IA parte 4

--- app/src/main/java/com/marcoantonioaav/lobogames/player/agent/Agent.java
+++ app/src/main/java/com/marcoantonioaav/lobogames/player/agent/Agent.java
@@ -9,14 +9,19 @@ public abstract class Agent extends Player {
     public static String MEDIUM_DIFFICULTY = "Médio";
     public static String HARD_DIFFICULTY = "Difícil";
 
+    private String difficulty = MEDIUM_DIFFICULTY;
+
     public Agent(int id, String difficulty) {
         super(id);
+        setDifficulty(difficulty);
     }
 
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+
+    protected String getDifficulty() { return difficulty; }
+
     @Override
-    public boolean isReady() {
-        return true;
-    }
+    public boolean isReady() { return true; }
 
     @Override
     public final Move getMove(Game game, int[][] board) {
